855bdd05c125589d8bca7f8e8a9052ba8789b2b8
davidcloss <davidcloss@live.com>
2024-02-04 10:04:18 -0300

    categorias_financeiras

--- exclusao_donwloads.py
+++ exclusao_donwloads.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 arquivos = glob(r'C:\Users\david\Downloads\*.*', recursive=True)
-
+data_inicio = datetime.now()
 for arquivo in arquivos:
     epoch = os.path.getatime(arquivo)
     data = datetime.fromtimestamp(epoch)
@@ -12,3 +12,8 @@ for arquivo in arquivos:
         os.remove(arquivo)
     else:
         pass
+
+data_fim = datetime.now()
+
+with open('hist.txt', 'a', encoding='UTF-8') as arquivo:
+    arquivo.write(f'{data_inicio}|{data_fim}\n')
